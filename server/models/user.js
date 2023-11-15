492a2501291c0e7450a19b013479d82df155f15a
mar69287 <marcoa.ruiz97@gmail.com>
2023-11-14 20:41:01 -0800

    update user model

--- server/models/user.js
+++ server/models/user.js
@@ -1,5 +1,5 @@
-const mongoose = require('mongoose');
-const Schema = mongoose.Schema;
+import mongoose from 'mongoose';
+const { Schema } = mongoose;
 
 const userSchema = new Schema({
     name: {type: String, required: true},
@@ -18,4 +18,4 @@ const userSchema = new Schema({
     timestamps: true,
 });
 
-module.exports = mongoose.model('User', userSchema);
+export default mongoose.model('User', userSchema);
