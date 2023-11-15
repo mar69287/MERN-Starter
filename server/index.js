a320761151af8a9ba2c396894aa3f4425f5c55c8
mar69287 <marcoa.ruiz97@gmail.com>
2023-11-14 19:34:45 -0800

    connect database

--- server/index.js
+++ server/index.js
@@ -2,6 +2,8 @@ import express from 'express';
 import * as dotenv from 'dotenv';
 import cors from 'cors';
 
+import connectDB from './config/database.js';
+
 dotenv.config();
 
 const app = express();
@@ -11,9 +13,15 @@ app.use(express.json());
 app.get('/', async (req, res) => {
     res.send('hello from Server Side!')
 });
+const port = process.env.PORT || 8080;
 
 const startServer = async () => {
-    app.listen(8080, () => console.log('Server started on port port http://localhost:8080'));
+    try {
+      connectDB(process.env.DATABASE_URL);
+      app.listen(8080, () => console.log(`Express app running on port ${port}`));
+    } catch (error) {
+      console.log(error);
+    }
 };
-
-startServer()
+  
+startServer();
