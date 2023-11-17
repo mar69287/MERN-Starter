6615ea57d4e62f77234cdfca6dd85c5bd5bebfec
mar69287 <marcoa.ruiz97@gmail.com>
2023-11-17 14:49:00 -0800

    add morgan

--- server/index.js
+++ server/index.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import * as dotenv from 'dotenv';
 import cors from 'cors';
+import morgan from 'morgan';
 
 import connectDB from './config/database.js';
 import userRoutes from './routes/userRoutes.js'
@@ -10,6 +11,7 @@ dotenv.config();
 const app = express();
 app.use(cors());
 app.use(express.json());
+app.use(morgan('dev'));
 
 app.use('/api/v1/users', userRoutes)
 
